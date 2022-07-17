d2703393f5aa10cbed1b24a3daa8d34df9b58988
luanheuripedes <luanheuripedesmoreiradepaula@gmail.com>
2022-07-17 13:04:19 -0300

    Adicionado toda as libs na pasta src

--- src/DevFreela.Core/Entities/UserSkill.cs
+++ src/DevFreela.Core/Entities/UserSkill.cs
@@ -9,10 +9,7 @@ namespace DevFreela.Core.Entities
     public class UserSkill:BaseEntity
     {
         public int IdUser { get; private set; }
-        public int IdSkill
-        {
-            get; private set;
-        }
+        public int IdSkill { get; private set; }
 
         public UserSkill(int idUser, int idSkill)
         {
