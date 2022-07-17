ff79d6a8374dbe89ac1c6d3c24f231d2babf292a
luanheuripedes <luanheuripedesmoreiradepaula@gmail.com>
2022-07-17 14:11:18 -0300

    Adicionado SkillsService

---
+++ src/DevFreela.Application/Services/Implementations/SkillService.cs
@@ -0,0 +1,27 @@
+﻿using DevFreela.Application.InputModels;
+using DevFreela.Application.Services.Interfaces;
+using DevFreela.Infrastructure.Persistence;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DevFreela.Application.Services.Implementations
+{
+    public class SkillService : ISkillService
+    {
+        private readonly DevFreelaDbContext _contex;
+
+        public SkillService(DevFreelaDbContext contex)
+        {
+            _contex = contex;
+        }
+
+        public List<SkillViewModel> GetAll()
+        {
+            var skills = _contex.Skills.Select(s => new SkillViewModel(s.Id,s.Description)).ToList();
+            return skills;
+        }
+    }
+}
--- src/DevFreela.Application/ViewModels/ProjectViewModel.cs
+++ src/DevFreela.Application/ViewModels/ProjectViewModel.cs
@@ -8,12 +8,14 @@ namespace DevFreela.Application.ViewModels
 {
     public class ProjectViewModel
     {
-        public ProjectViewModel(string title, DateTime createdAt)
+        public ProjectViewModel(string title, DateTime createdAt, int id)
         {
             Title = title;
             CreatedAt = createdAt;
+            Id = id;
         }
 
+        public int Id { get; set; }
         public string Title { get; private set; }
         public DateTime CreatedAt { get; private set; }
     }
