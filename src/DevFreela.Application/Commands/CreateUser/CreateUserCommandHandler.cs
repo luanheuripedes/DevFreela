a3beac1658147f286a8442cbf5c78d0e691afd15
luanheuripedes <luanheuripedesmoreiradepaula@gmail.com>
2022-07-27 14:04:11 -0300

    Salvando a Senha de Maneira Segura, criado um metodo que computa a senha em hash256

--- src/DevFreela.Application/Commands/CreateUser/CreateUserCommandHandler.cs
+++ src/DevFreela.Application/Commands/CreateUser/CreateUserCommandHandler.cs
@@ -1,5 +1,6 @@
 ﻿using DevFreela.Core.Entities;
 using DevFreela.Core.IRepositories;
+using DevFreela.Core.Services;
 using MediatR;
 using System;
 using System.Collections.Generic;
@@ -13,15 +14,20 @@ namespace DevFreela.Application.Commands.CreateUser
     {
 
         private readonly IUserRepository _repository;
+        private readonly IAuthService _authService;
 
-        public CreateUserCommandHandler(IUserRepository repository)
+        public CreateUserCommandHandler(IUserRepository repository, IAuthService authService)
         {
             _repository = repository;
+            _authService = authService;
         }
 
         public async Task<int> Handle(CreateUserCommand request, CancellationToken cancellationToken)
         {
-            var user = new User(request.Name, request.Email, request.BirthDate,request.Password, request.Role);
+
+            var passwordHash = _authService.ComputeSha256Hash(request.Password);
+
+            var user = new User(request.Name, request.Email, request.BirthDate, passwordHash, request.Role);
             await _repository.CreateUserAsync(user);
 
             return user.Id;
