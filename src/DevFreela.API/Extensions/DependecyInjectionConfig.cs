5366659dca1a3ef676f82ac51b2ff5740c48af1e
luanheuripedes <luanheuripedesmoreiradepaula@gmail.com>
2022-08-16 17:31:59 -0300

    Adicionado uma classe para fazer as injecçoes de dependencia

---
+++ src/DevFreela.API/Extensions/DependecyInjectionConfig.cs
@@ -0,0 +1,44 @@
+﻿using DevFreela.Application.Commands.CreateProject;
+using DevFreela.Application.Services.Implementations;
+using DevFreela.Application.Services.Interfaces;
+using DevFreela.Core.IRepositories;
+using DevFreela.Core.Repositories;
+using DevFreela.Core.Services;
+using DevFreela.Infrastructure.Auth;
+using DevFreela.Infrastructure.Payments;
+using DevFreela.Infrastructure.Persistence;
+using DevFreela.Infrastructure.Persistence.Repositories;
+using DevFreela.Infrastructure.Repositories;
+using MediatR;
+
+namespace DevFreela.API.Extensions
+{
+    public static class DependecyInjectionConfig
+    {
+        public static IServiceCollection ResolveDependencies(this IServiceCollection services)
+        {
+
+            //Adiciona o MediatR
+            //busca no Assembly Application todas as classes que implementem IRequest<> 
+            //e associar a todos o commands handler que implementam IRequestHandler<>
+            services.AddMediatR(typeof(CreateProjectCommand));
+
+            //para construir instancias do httpClient em varias partes do sistema para fazer requisições fora
+            services.AddHttpClient();
+
+            //Repositories
+            services.AddScoped<IProjectRepository, ProjectRepository>();
+            services.AddScoped<IUserRepository, UserRepository>();
+            services.AddScoped<ISkillRepository, SkillRepository>();
+
+            //Services
+            services.AddScoped<IUsersService, UsersService>();
+            services.AddScoped<IAuthService, AuthService>();
+            services.AddScoped<IPaymentService, PaymentService>();
+
+
+
+            return services;
+        }
+    }
+}
--- src/DevFreela.API/Program.cs
+++ src/DevFreela.API/Program.cs
@@ -1,3 +1,4 @@
+using DevFreela.API.Extensions;
 using DevFreela.API.Filters;
 using DevFreela.Application.Commands.CreateProject;
 using DevFreela.Application.Services.Implementations;
@@ -34,23 +35,9 @@ builder.Configuration.GetValue<string>("Env");   //Aqui
 var connectionString = builder.Configuration.GetConnectionString("DevFreelaCs");
 builder.Services.AddDbContext<DevFreelaDbContext>(options => options.UseSqlServer(connectionString));
 
-//para construir instancias do httpClient em varias partes do sistema para fazer requisições fora
-builder.Services.AddHttpClient();
 
-builder.Services.AddScoped<IUsersService, UsersService>();
-builder.Services.AddScoped<IAuthService, AuthService>();
-builder.Services.AddScoped<IPaymentService, PaymentService>();
+builder.Services.ResolveDependencies();
 
-
-
-//Adiciona o MediatR
-//busca no Assembly Application todas as classes que implementem IRequest<> 
-//e associar a todos o commands handler que implementam IRequestHandler<>
-builder.Services.AddMediatR(typeof(CreateProjectCommand));
-
-builder.Services.AddScoped<IProjectRepository, ProjectRepository>();
-builder.Services.AddScoped<IUserRepository, UserRepository>();
-builder.Services.AddScoped<ISkillRepository, SkillRepository>();
 
 //Configuração do fluentValidator e do validation Filter
 builder.Services.AddControllers(options =>
